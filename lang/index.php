f507c3a0f95d6126b132fcdc7cc784db4342dbd1
TheEl0ja <theel0ja@theel0ja.info>
2015-09-16 17:30:04 +0300

    Create index.php

---
+++ lang/index.php
@@ -0,0 +1,16 @@
+<?php
+  /*
+    DO NOT TOUCH!
+  */
+  
+  header("Location: ../");
+?>
+<h1>Something went wrong.</h1>
+
+<p>
+  Do you have PHP and .htaccess enabled? If not, please enable.
+</p>
+
+<footer>
+  Powered by: <a href="https://github.com/theel0ja/CKalMS">CKalMS</a>
+</footer>
