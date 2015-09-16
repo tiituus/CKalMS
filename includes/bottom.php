9a64eef284fc8e1886fbc65b5338ac7453f03e8e
TheEl0ja <theel0ja@theel0ja.info>
2015-09-16 17:25:33 +0300

    Update bottom.php

--- includes/bottom.php
+++ includes/bottom.php
@@ -1,6 +1,12 @@
   <footer>
-  <span class="left">
-  &copy; <?php echo $sitename; ?>
-  Layout made by: <a href="https://github.com/theel0ja">TheEl0ja</a> 
+    <span class="left">
+      &copy; <?php echo $sitename; ?>
+    </span>
+    <span class="right">
+      <!-- Layout made by: <a href="https://github.com/author">Author Name</a> &bull; -->
+    <!-- ^ You can add if you want. ^ -->
+        Powered by: <a href="https://github.com/theel0ja/CKalMS">CKalMS</a>
+    </span>
+  </footer>
   </body>
 </html>
