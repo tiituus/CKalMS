fd32877a8717c86bae3b68fa525ba55cef0339ab
TheEl0ja <theel0ja@theel0ja.info>
2015-09-16 17:21:50 +0300

    Create top.php

---
+++ includes/top.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    
+    <title><?php echo $title; ?></title>
+    
+    <!-- Internet Explorer Bug Fixing -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+		<!--[if lt IE 9]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
